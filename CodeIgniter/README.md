7526cb7c9a4ccf2761d5d92534e9d2a8e466eeb3
2301020 <147363650+2301020@users.noreply.github.com>
2024-05-19 23:18:34 +0800

    Uploaded CodeIgniter
    added codeigniter files and readme instructions

---
+++ CodeIgniter/README.md
@@ -0,0 +1,8 @@
+# CSC1106 Tutorial CodeIgniter 
+
+## Step 0: Download Prerequisites
+- Install php and MySQL, installing via XAMPP makes this pretty convenient: https://www.apachefriends.org/download.html
+- One common issue is the missing/deactivated 'intl' extension, navigate to C:\xampp\php\php.ini (or wherever the php.ini file is) and uncomment 'extension=intl' (remove the ';' from ';extension=intl')
+
+## Step 1: Run Application
+```php spark serve```
---
+++ CodeIgniter/codeigniter4-framework-v4.5.1-0-g9b2cd73/app/Config/Routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Pages;
+use App\Controllers\News; // Add this line
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/new', [News::class, 'new']); // Add this line
+$routes->post('news', [News::class, 'create']); // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
+
